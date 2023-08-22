bb719f4f4caad6a54fcfc97ed6e0f0682810ec7f
clockwork <mjahan53@proton.me>
2023-08-22 23:13:05 +0330

    - add matrix inverse

--- src/matrix.rs
+++ src/matrix.rs
@@ -2,8 +2,9 @@
 // #![feature(generic_const_exprs, adt_const_params, const_generics, const_evaluatable_checked)]
 
 use std::fmt::Debug;
-use std::ops::{Add, Mul, Neg, Sub};
+use std::ops::{Add, Mul, Div, Neg, Sub};
 use crate::tuple::Tuple;
+use crate::vec3::EPSILON;
 
 #[allow(dead_code)]
 #[repr(usize)]
@@ -13,12 +14,13 @@ enum MatrixSize {
 	_4X4 = 4,
 }
 
+#[allow(dead_code)]
 pub enum MatrixFill<T, const N: usize> {
 	Array([[T; N]; N]),
 	Single(T),
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 pub struct Matrix<T, const N: usize> {
 	data: [[T; N]; N],
 }
@@ -88,6 +90,7 @@ macro_rules! impl_matrix_n_x_n {
 				self.get(0, 0) * self.get(1, 1) - self.get(0, 1) * self.get(1, 0)
 			}
 
+			#[allow(dead_code)]
 			pub fn invertible(&self) -> bool {
 				self.determinant() != T::default()
 			}
@@ -96,7 +99,8 @@ macro_rules! impl_matrix_n_x_n {
     ($size:expr) => {
         impl<T> Matrix<T, $size>
         where
-            T: Default + Copy + Mul<Output = T> + Sub<Output = T> + Add<Output = T> + Neg<Output = T> + PartialEq,
+            T: Default + Copy + Mul<Output = T> + Div<Output=T> +
+                Sub<Output = T> + Add<Output = T> + Neg<Output = T> + PartialEq, f64: From<T>, T: std::fmt::Debug
         {
             pub fn sub_matrix(&self, row: usize, col: usize) -> Matrix<T, {$size - 1}> {
                 let mut sub_matrix = Matrix::new(None);
@@ -123,7 +127,7 @@ macro_rules! impl_matrix_n_x_n {
             }
 
             pub fn cofactor(&self, row: usize, col: usize) -> T {
-                if row + col % 2 == 0 {
+                if (row + col) % 2 == 0 {
                     self.minor(row, col)
                 } else {
                     -self.minor(row, col)
@@ -138,9 +142,27 @@ macro_rules! impl_matrix_n_x_n {
                 result
             }
 
+            #[allow(dead_code)]
             pub fn invertible(&self) -> bool {
                 self.determinant() != T::default()
             }
+
+            #[allow(dead_code)]
+            pub fn inverse(&self) -> Option<Matrix<f64, $size>> {
+				if !self.invertible() {
+					return None;
+				}
+				let mut res = Matrix::new(None);
+	            for row in 0..$size {
+					for col in 0..$size {
+						let cof = f64::try_from(self.cofactor(row, col)).unwrap();
+						let det = f64::try_from(self.determinant()).unwrap();
+						// dbg!(row, col, cof, det, cof / det);
+						res.set(col, row, cof / det);
+					}
+				}
+	            Some(res)
+			}
         }
     };
 }
@@ -150,8 +172,27 @@ impl_matrix_n_x_n!(3);
 impl_matrix_n_x_n!(4);
 
 
+impl<T, const N: usize> PartialEq for Matrix<T, N>
+	where T: Default + Copy, f64: From<T>
+{
+	fn eq(&self, other: &Self) -> bool {
+		for i in 0..N {
+			for j in 0..N {
+				if (
+					f64::try_from(other.get(i, j)).unwrap() -
+						f64::try_from(self.get(i, j)).unwrap()
+				).abs() > EPSILON as f64 {
+					return false;
+				}
+			}
+		}
+		true
+	}
+}
+
 // NxN
 impl<T: Default + Copy, const N: usize> Matrix<T, N> {
+	#[allow(dead_code)]
 	const CHECK: () = assert!(N >= 2 && N <= 4);
 
 	pub fn new(values: Option<MatrixFill<T, N>>) -> Matrix<T, N> {
@@ -277,7 +318,7 @@ mod tests {
 			[40, 58, 110, 102],
 			[16, 26, 46, 42]
 		];
-		assert_eq!(matrix4x4_filled_a.mul(matrix4x4_filled_b).data, fill_array_c)
+		assert_eq!((matrix4x4_filled_a * matrix4x4_filled_b).data, fill_array_c)
 	}
 
 	#[test]
@@ -294,7 +335,7 @@ mod tests {
 		let tuple4x1_filled_b = Tuple::new(Some(TupleFill::Array(fill_array_b)));
 
 		let fill_array_c: [i64; 4] = [18, 24, 33, 1];
-		assert_eq!(matrix4x4_filled_a.mul(tuple4x1_filled_b).data, fill_array_c)
+		assert_eq!((matrix4x4_filled_a * tuple4x1_filled_b).data, fill_array_c)
 	}
 
 	#[test]
@@ -313,13 +354,13 @@ mod tests {
 		];
 		let matrix4x4_filled_a = Matrix::new(Some(MatrixFill::Array(fill_array_a)));
 		let matrix4x4_filled_c = Matrix::new(Some(MatrixFill::Array(fill_array_c)));
-		assert_eq!(matrix4x4_filled_a.mul(IDENTITY_MATRIX).data, fill_array_a);
-		assert_eq!(matrix4x4_filled_c.mul(IDENTITY_MATRIX).data, fill_array_c);
+		assert_eq!((matrix4x4_filled_a * IDENTITY_MATRIX).data, fill_array_a);
+		assert_eq!((matrix4x4_filled_c * IDENTITY_MATRIX).data, fill_array_c);
 
 		let fill_array_b: [i64; 4] = [1, 2, 3, 1];
 		let tuple4x1_filled_b = Tuple::new(Some(TupleFill::Array(fill_array_b)));
 
-		assert_eq!(IDENTITY_MATRIX.mul(tuple4x1_filled_b).data, fill_array_b)
+		assert_eq!((IDENTITY_MATRIX * tuple4x1_filled_b).data, fill_array_b)
 	}
 
 	#[test]
@@ -489,5 +530,123 @@ mod tests {
 		assert_eq!(matrix.cofactor(0, 0), -12);
 		assert_eq!(matrix.minor(1, 0), 25);
 		assert_eq!(matrix.cofactor(1, 0), -25);
+	}
+
+	#[test]
+	fn test_invertible_matrix_4x4() {
+		let matrix = Matrix::new(Some(MatrixFill::Array([
+			[6, 4, 4, 4],
+			[5, 5, 7, 6],
+			[4, -9, 3, -7],
+			[9, 1, 7, -6]
+		])));
+
+		assert_eq!(matrix.invertible(), true);
+	}
+
+	#[test]
+	fn test_non_invertible_matrix_4x4() {
+		let matrix = Matrix::new(Some(MatrixFill::Array([
+			[-4, 2, -2, -3],
+			[9, 6, 2, 6],
+			[0, -5, 1, -5],
+			[0, 0, 0, 0]
+		])));
+
+		assert_eq!(matrix.invertible(), false);
+	}
+
+	#[test]
+	fn test_inverse_matrix_4x4_a() {
+		let matrix = Matrix::new(Some(MatrixFill::Array([
+			[-5, 2, 6, -8],
+			[1, -5, 1, 8],
+			[7, 7, -6, -7],
+			[1, -3, 7, 4]
+		])));
+
+		let inverse = Matrix::new(Some(MatrixFill::Array([
+			[0.21805, 0.4511278, 0.24060150, -0.045112781],
+			[-0.808270, -1.45676, -0.443609, 0.5206766917],
+			[-0.078947, -0.2236842, -0.0526315, 0.19736842],
+			[-0.522556, -0.81390, -0.3007518, 0.30639097]
+		])));
+
+		assert_eq!(matrix.invertible(), true);
+		let b = matrix.inverse().unwrap();
+
+		assert_eq!(matrix.determinant(), 532);
+		//
+		assert_eq!(matrix.cofactor(1, 1), -775);
+		assert_eq!(b.get(1, 1), -775. / 532.);
+		//
+		assert_eq!(matrix.cofactor(2, 3), -160);
+		assert_eq!(b.get(3, 2), -160. / 532.);
+		//
+		assert_eq!(matrix.cofactor(3, 2), 105);
+		assert_eq!(b.get(2, 3), 105. / 532.);
+		assert_eq!(b, inverse);
+	}
+
+	#[test]
+	fn test_inverse_matrix_4x4_b() {
+		let matrix = Matrix::new(Some(MatrixFill::Array([
+			[8, -5, 9, 2],
+			[7, 5, 6, 1],
+			[-6, 0, 9, 6],
+			[-3, 0, -9, -4]
+		])));
+
+		let inverse = Matrix::new(Some(MatrixFill::Array([
+			[-0.15384615384615385, -0.15384615384615385, -0.28205128205128205, -0.5384615384615384],
+			[-0.07692307692307693, 0.12307692307692308, 0.02564102564102564, 0.03076923076923077],
+			[0.358974358974359, 0.358974358974359, 0.4358974358974359, 0.9230769230769231],
+			[-0.6923076923076923, -0.6923076923076923, -0.7692307692307693, -1.9230769230769231]
+		])));
+
+		assert_eq!(matrix.invertible(), true);
+		let b = matrix.inverse().unwrap();
+
+		assert_eq!(b, inverse);
+	}
+
+	#[test]
+	fn test_inverse_matrix_4x4_c() {
+		let matrix = Matrix::new(Some(MatrixFill::Array([
+			[9, 3, 0, 9],
+			[-5, -2, -6, -3],
+			[-4, 9, 6, 4],
+			[-7, 6, 6, 2]
+		])));
+
+		let inverse = Matrix::new(Some(MatrixFill::Array([
+			[-0.040740740740740744, -0.07777777777777778, 0.14444444444444443, -0.2222222222222222],
+			[-0.07777777777777778, 0.03333333333333333, 0.36666666666666664, -0.3333333333333333],
+			[-0.029012345679012345, -0.14629629629629629, -0.10925925925925926, 0.12962962962962962],
+			[0.17777777777777778, 0.06666666666666667, -0.26666666666666666, 0.3333333333333333]
+		])));
+
+		assert_eq!(matrix.invertible(), true);
+		let b = matrix.inverse().unwrap();
+
+		assert_eq!(b, inverse);
+	}
+
+	#[test]
+	fn test_inverse_matrix_4x4_d() {
+		let matrix_a = Matrix::new(Some(MatrixFill::Array([
+			[3, -9, 7, 3],
+			[3, -8, 2, -9],
+			[-4, 4, 4, 1],
+			[-6, 5, -1, 1]
+		])));
+		let matrix_b = Matrix::new(Some(MatrixFill::Array([
+			[8, 2, 2, 2],
+			[3, -1, 7, 0],
+			[7, 0, 5, 4],
+			[6, -2, 0, 5]
+		])));
+		let matrix_c = matrix_a * matrix_b;
+		assert_eq!(matrix_c * matrix_b.inverse().unwrap(), matrix_a);
 	}
 }
--- src/tuple.rs
+++ src/tuple.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 pub enum TupleFill<T> {
 	Array([T; 4]),
 	Single(T),
